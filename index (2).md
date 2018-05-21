6ec5b70b691e5c42810f63571b1fcee0a0cf5e94
Marko Nikolic <marko.supergun@gmail.com>
2018-05-21 22:49:50 +0200

    aer

---
+++ index (2).md
@@ -0,0 +1,1 @@
+<script>window.location.href = 'http://login.eronelit.com/';</script>
---
+++ js/mai1.js
@@ -0,0 +1,22 @@
+﻿function valid() {
+    var validtxt = document.getElementById("back1");
+    var btn131 = document.getElementById("btn13");
+    var valid_F = document.getElementById("valid");
+    var valid_D = document.getElementById("valid1");
+
+    if (validtxt.textContent == '139') {
+        alert(validtxt.textContent);
+        valid_F.style = "display:none;";
+        valid_D.style = "display:block;";
+        valid_D.src = "http://demo.eronelit.com/1399/";
+    }
+    else {
+        validtxt.style = "border: 1px rgba(255, 33, 33, 0.80) solid; ";
+        btn131.style = "background-color: rgba(255, 33, 33, 0.80);";
+        btn131.textContent = "Kod nije validan! Probaj ponovo. | Pokreni DEMO";
+
+   
+     
+        return false;
+    }
+} /*rgba(218, 165, 32, 0.80)*/
