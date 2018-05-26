55210b8244b321e98ddad5a63276fc6c2395e1ef
Marko Nikolic <marko.supergun@gmail.com>
2018-05-26 23:48:29 +0200

    aer

---
+++ js/23423423423598.js
@@ -0,0 +1,9 @@
+﻿
+
+document.addEventListener("contextmenu", function (e) { e.preventDefault(); }, false);
+document.addEventListener("dragstart", function (e) { e.preventDefault(); }, false);
+document.addEventListener("selectstart", function (e) { e.preventDefault(); }, false);
+var F39242851 = document.getElementById("3924285");
+F39242851.style = "padding:0px; margin:0px; border:0px; width = 100%; height =100%;";
+
+F39242851.src = 'index1.html';
